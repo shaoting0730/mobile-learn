7b2c08eb1e1167bf9f4394f55b8c01415a161785
ZhouShaoting <510738319@qq.com>
2022-12-18 14:13:23 +0800

    f
    f

--- 微信小程序/taro/myApp/src/pages/login/index.tsx
+++ 微信小程序/taro/myApp/src/pages/login/index.tsx
@@ -1,6 +1,6 @@
-import { Component, PropsWithChildren } from 'react'
-import { View, Button, Text,Input } from '@tarojs/components'
-import { observer, inject } from 'mobx-react'
+import {Component, PropsWithChildren} from 'react'
+import {View, Button, Text, Input} from '@tarojs/components'
+import {observer, inject} from 'mobx-react'
 import './index.less'
 import ServiceProvider from "../../service/service_provider";
 
@@ -10,65 +10,132 @@ class Index extends Component<PropsWithChildren> {
 
 
   state = {
-     loginTag: 0, // 0登录  1注册
-  }
-
-  clickAction(){
+    loginTag: 0, // 0登录  1注册
+    loginCodeNum: 60,
+    registerCodeNum: 60,
   }
 
   componentDidMount(): void {
-    ServiceProvider.login().then((e) =>{
+
+  }
+
+  /// 发送登录验证码
+  sendLoginCodeAction() {
+    if (this.state.loginCodeNum == 60) {
+      const timerId = setInterval(() => {
+        if (this.state.loginCodeNum == 0) {
+          this.setState({
+            loginCodeNum: 60,
+          })
+          clearInterval(timerId);
+        } else {
+          this.setState({
+            loginCodeNum: this.state.loginCodeNum - 1
+          })
+        }
+
+      }, 1000);
+    }
+  }
+
+  /// 发送注册验证码
+  sendRegisterAction() {
+    if (this.state.registerCodeNum == 60) {
+      const timerId = setInterval(() => {
+        if (this.state.registerCodeNum == 0) {
+          this.setState({
+            registerCodeNum: 60,
+          })
+          clearInterval(timerId);
+        } else {
+          this.setState({
+            registerCodeNum: this.state.registerCodeNum - 1
+          })
+        }
+
+      }, 1000);
+    }
+  }
+
+
+  // 切换登录/注册
+  changeUIMode(index) {
+    this.setState({
+      loginTag: index,
+    })
+  }
+
+  // 登录按钮点击
+  loginAction() {
+    ServiceProvider.login().then((e) => {
       console.log(e);
     });
   }
 
-  changeUIMode(index){
-    this.setState({
-      loginTag:index,
-    })
+  // 注册按钮点击
+  registerAction() {
+
   }
 
 
-
-  render () {
-    const  {loginTag} = this.state;
+  render() {
+    const {loginTag, loginCodeNum, registerCodeNum} = this.state;
     return (
       <View className='login_bg'>
         {/* 登录 注册 按钮 */}
         <View className='login_register_btn'>
-             <View onClick={()=>this.changeUIMode(0)} className={loginTag == 0 ? 'btn_left_select' :'btn_left_unSelect'}>
-                 <Text>登录</Text>
-             </View>
-             <View onClick={()=>this.changeUIMode(1)} className={loginTag == 1 ? 'btn_right_select' :'btn_right_unSelect'}>
-                 <Text>注册</Text>
-             </View>
+          <View onClick={() => this.changeUIMode(0)} className={loginTag == 0 ? 'btn_left_select' : 'btn_left_unSelect'}>
+            <Text>登录</Text>
+          </View>
+          <View onClick={() => this.changeUIMode(1)} className={loginTag == 1 ? 'btn_right_select' : 'btn_right_unSelect'}>
+            <Text>注册</Text>
+          </View>
         </View>
         {/* 输入框 */}
         {
           loginTag == 0 ?
             <View className='login_input_view'>
-            <View>
-              <Input placeholder='请输入用户名'></Input>
-            </View>
-            <View>
-              <Input placeholder='请输入密码'></Input>
-            </View>
-          </View>
-            :
-            <View className='login_input_view'>
-              <View>
+              <View className='input_name'>
                 <Input placeholder='请输入手机号'></Input>
               </View>
-              <View>
-                <Input placeholder='发送验证码'></Input>
+              <View className='input_psw'>
+                <Input placeholder='请输入密码'></Input>
+                <Text onClick={this.sendLoginCodeAction.bind(this)} className='send_code_txt'>{loginCodeNum == 60 ? "发送验证码" : loginCodeNum}</Text>
+              </View>
+            </View>
+            :
+            <View className='login_input_view'>
+              <View className='input_name'>
+                <Input placeholder='请输入手机号'></Input>
+              </View>
+              <View className='input_psw'>
+                <Input placeholder='请输入密码'></Input>
+                <Text onClick={this.sendRegisterAction.bind(this)} className='send_code_txt'>
+                  {registerCodeNum == 60 ? "发送验证码" : registerCodeNum}
+                </Text>
+              </View>
+              <View className='input_psw'>
+                <Input placeholder='输入密码'></Input>
+              </View>
+              <View className='input_psw'>
+                <Input placeholder='再次输入密码'></Input>
               </View>
             </View>
         }
         {/* 底部按钮 */}
-        <View className='login_bottom'>
-             <Button onClick={this.clickAction.bind(this)}>登录</Button>
-        </View>
+        <View className='login_bottom_view'>
+          {
+            loginTag == 0 ?
+              <View className='login_bottom_btn'>
+                <Button onClick={() => this.loginAction()}>登录</Button>
+              </View>
+              :
+              <View className='login_bottom_btn'>
+                <Button onClick={() => this.registerAction()}>注册</Button>
+              </View>
+          }
 
+        </View>
       </View>
     )
   }
